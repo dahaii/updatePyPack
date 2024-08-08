0e77c4d3ab91c7c18d8d23e24761d1adcbbc2ed3
Enes <95295825+dahaii@users.noreply.github.com>
2024-08-08 13:18:02 +0300

    Add files via upload

---
+++ updatePyPack/update_packages.py
@@ -0,0 +1,31 @@
+import subprocess
+import re
+
+def get_outdated_packages():
+    result = subprocess.run(['pip', 'list', '--outdated'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    lines = output.splitlines()
+    outdated_packages = []
+    for line in lines[2:]:  # İlk iki satır başlık bilgisi içerir
+        parts = re.split(r'\s+', line)
+        if len(parts) >= 4:
+            outdated_packages.append(parts[0])
+    return outdated_packages
+
+def update_package(package):
+    subprocess.run(['pip', 'install', '--upgrade', package])
+
+def main():
+    outdated_packages = get_outdated_packages()
+    for package in outdated_packages:
+        update_package(package)
+        print(f'{package} güncellendi.')
+
+if __name__ == '__main__':
+    main()
+
+# cmd yönetici ile çalıştır 
+# "cd .." ile dizinden çıkıp "cd" ile dizine git
+# C:\Users\enesi\OneDrive - Akdeniz Üniversitesi\Masaüstü\Python>cd updatingPyPack
+# python update_packages.py
+# calıstır
